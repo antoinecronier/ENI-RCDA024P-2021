58cc963ad7c4fe73acbbf46bf2488cae9fcd79ca
harckline <antoinecronier1@gmail.com>
2021-04-21 09:35:22 +0200

    Progress module5 demo1

---
+++ Module4-Demo1/Controllers/DemoController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Module4_Demo1.Controllers
+{
+    public class DemoController : Controller
+    {
+        // GET: Demo
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Mon message";
+            return View();
+        }
+
+        public ActionResult Test1()
+        {
+            TempData["key1"] = "valeur de key1";
+
+            return View();
+        }
+    }
+}
